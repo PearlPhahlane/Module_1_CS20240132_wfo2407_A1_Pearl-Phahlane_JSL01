a0655e1b2f6f1a13497eddc5e2d2e5409d783d81
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-08-26 14:02:29 +0200

    separate number and letters array to avoid adding the name then pets birth year turning to valid syntax

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     let result = ''; // Placeholder for validation result
 
-// Regular expression to match the required syntax: "pet_" followed by letters and numbers
-const regex = /^pet_[a-zA-Z0-9]+$/;
+// Regular expression to match the required syntax: "pet_" followed by letters and numbers.
+//Make sure to separate the number and letters array are separate and concatenated
+const regex = /^pet_[0-9]+[a-zA-Z]+$/;
 
 // Check if input matches the regex pattern
 if (regex.test(input)) {
