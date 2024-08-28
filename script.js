950f1d7043a887ca68178247567cd87b7b934041
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-08-28 14:41:07 +0200

    fix second comment for code readability

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
 // Regular expression to match the required syntax: "pet_" followed by letters and numbers.
-//Make sure to separate the number and letters array are separate and concatenated
+//Make sure to separate the number and letters array and also concatenate
 const regex = /^pet_[0-9]+[a-zA-Z]+$/;
 
 // Check if input matches the regex pattern
