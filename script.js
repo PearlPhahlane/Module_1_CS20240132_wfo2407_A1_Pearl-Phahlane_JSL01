ced32b14f7b12989345a59ab594fb8747068ac37
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-08-29 16:33:06 +0200

    fix regex to regEx for correct naming in camelCase

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function validateSyntax() {
 
 // Regular expression to match the required syntax: "pet_" followed by letters and numbers.
 //Make sure to separate the number and letters array and also concatenate
-const regex = /^pet_[0-9]+[a-zA-Z]+$/;
+const regEx = /^pet_[0-9]+[a-zA-Z]+$/;
 
 // Check if input matches the regex pattern
 if (regex.test(input)) {
