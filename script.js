04fafa59f704323d131a7ef6e92312fed8cf3539
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-08-29 17:06:13 +0200

    change from single to double quotes in the let declarations

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function validateSyntax() {
-  let input = document.getElementById('petInput').value;
-  let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  let result = ""; // Placeholder for validation result
 
   // Regular expression to match the required syntax: "pet_" followed by letters and numbers.
   //Make sure to separate the number and letters array and also concatenate
   const regEx = /^pet_[0-9]+[a-zA-Z]+$/;
 
   // Check if input matches the regex pattern
-  if (regex.test(input)) {
-    result = "Valid Syntax";
+  if (regEx.test(input)) {
+    result = "Valid Syntax \u{1F7E2}"; //Green filled circle emoji for valid syntax
   } else {
-    result = "Invalid Syntax";
+    result = "Invalid Syntax \u{1F534}"; //Red filled circle emoji for invalid syntax
   }
 
   document.getElementById("result").innerText = result;
